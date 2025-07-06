08a97b7bed8b1881778d459de6bc5c010a5cef7e
wuzheng <1657955621@qq.com>
2025-07-06 20:55:42 +0800

    🎉 init: 初始化，删除无用的文件

---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    首页
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
